5b28d8503fad1be3ac69e3d437d7f9cc3fc6ad0d
Khieran <100107960+Khieran@users.noreply.github.com>
2022-02-22 10:24:48 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+<title>Group 5 Main Web Page</title> 
+</head>
+<body>
+
+<body background="background.jpg" style="background-size: 100%">
+<font size="15">
+<h1 align =center><b>Web Page</h1>
+</font>
+<hr>
+<br>
+<br>
+<br>
+<font size="14"face="Bookman-Demi">
+<p align ="center"><b>CAE</p>
+<p align ="center"><font size="3"><i><b>Ms. Apple Agsam Egusquiza</p>
+</font>
+<br>
+<p align ="center><img src="cae.png" height="50"> 
+<hr>
+<br>
+<br>
+<br>
+
+<font size="14"face="Bookman-Demi">
+<p align ="center"><b>CLE</p>
+<p align ="center"><font size="3"><i><b>Sir. Karlo Villanueva</p>
+</font>
+<br>
+<p align ="center><img src="cle.png"height="50">
+<hr>
+<br>
+<br>
+<br>
+<font size="14"face="Bookman-Demi">
+<p align ="center"><b>MATH</p>
+<p align ="center"><font size="3"><i><b>Sir. Paquito Valor</p>
+</font>
+<br>
+<p align ="center><img src="math.png"height="50">
+<hr>
+<br>
+<br>
+<br>
+
+<font size="15">
+<p align ="center"><b>GROUP 5</p>
+<font size="10">
+<p align ="center">Estanol<br>Gallardo<br>De los Reyes<br>Java<br>Andag</p>
+
+</body>
+</html>
